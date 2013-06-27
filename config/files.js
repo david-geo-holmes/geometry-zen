f09346211a5f86516b672b7848efae3e4da65a8c
David Holmes <david.geo.holmes@gmail.com>
2013-06-27 00:38:12 -0400

    HTML5 Canvas arc

--- config/files.js
+++ config/files.js
@@ -34,7 +34,7 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
       "vendor/jquery-ui/ui/jquery-ui.js",
       "vendor/js/jquery.layout-1.3.0.js",
       "vendor/bladejs/build/blade.js",
-      "../davinci/build/davinci.min.js",
+      "../davinci/build/davinci.js",
       "../davinci/build/davinciLib.js",
       "vendor/bootstrap/js/bootstrap-carousel.js",
       "vendor/bootstrap/js/bootstrap-collapse.js",
